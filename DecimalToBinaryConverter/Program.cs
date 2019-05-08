84932c08fb866308113a39e89b382cac8a388dad
Brent Shearhart <shearhart2@gmail.com>
2019-05-08 19:26:44 -0400

    Beginning Decimal to Binary Converter

--- DecimalToBinaryConverter/Program.cs
+++ DecimalToBinaryConverter/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace DecimalToBinaryConverter
 {
-    class Program
+    class IntegerConvertToBinary
     {
+        public decimal userNumber { get; set; }
+        public int integerPortionOfUserNumber { get; set; }
+        public decimal fractionalPortionOfUserNumber { get; set; }
+
         static void Main(string[] args)
         {
+            IntegerConvertToBinary integerConvertToBinary = new IntegerConvertToBinary();
+            Console.WriteLine("Please enter a decimal to convert it to binary");
+            integerConvertToBinary.userNumber = Convert.ToDecimal(Console.ReadLine());
+            //TODO: write a method of catching inappropriate input
         }
+        
+        void SeparateIntegerAndFractionalPortionsOfUserNumber()
+        {
+            integerPortionOfUserNumber = (int)userNumber;
+            fractionalPortionOfUserNumber = userNumber - integerPortionOfUserNumber;
+        }
+
+        void ConvertIntegerPortionOfUserNumberToBinary()
+        {
+            List<int> ConversionResults = new List<int>();
+
+            do
+            {
+                ConversionResults.Insert(0, integerPortionOfUserNumber % 2);
+                integerPortionOfUserNumber /= 2;
+            } while (integerPortionOfUserNumber != 0);
+            //TODO: This should be all you need for the integer portion of the conversion, but go back over it before continuing
+        }
+        //TODO: Add method for ConvertFractionalPortionOfUserNumberToBinary
     }
 }
